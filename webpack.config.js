a31c472306f15ff773b9bd8667eff84377ccc444
Scott McDaniel <scott@hknapps.com>
2016-03-27 18:39:42 -0600

    move react to external dependencty

--- webpack.config.js
+++ webpack.config.js
@@ -10,6 +10,9 @@ var config = {
             test: /\.jsx?$/, // A regexp to test the require path. accepts either js or jsx
             loader: 'babel' // The module to load. "babel" is short for "babel-loader"
         }]
+    },
+    externals: {
+        'react': 'React'
     }
 };
 
