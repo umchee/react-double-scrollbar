7e662b64cffb3d5026981a22c50f760fd20d1cdb
Scott McDaniel <scott@hknapps.com>
2016-03-27 21:10:02 -0600

    still fighting webpack

--- webpack.config.js
+++ webpack.config.js
@@ -1,26 +1,35 @@
+var webpack = require('webpack');
 var path = require('path');
+
 var config = {
     entry: path.resolve(__dirname, 'src/DoubleScrollbar.jsx'),
+    devtool: 'source-map',
     output: {
         path: path.resolve(__dirname, 'dist'),
-        filename: 'index.js',
+        filename: 'DoubleScrollbar.js',
         library: "DoubleScrollbar",
-        libraryTarget: 'umd'
+        libraryTarget: 'umd',
+        umdNamedDefine: true
     },
     module: {
         loaders: [{
             test: /\.jsx?$/, // A regexp to test the require path. accepts either js or jsx
-            loader: 'babel' // The module to load. "babel" is short for "babel-loader"
+            loader: 'babel', // The module to load. "babel" is short for "babel-loader"
+            exclude: /(node_modules|bower_components)/
         }]
     },
-    externals: [{
+    resolve: {
+      root: path.resolve('./src'),
+      extensions: ['', '.js']
+    },
+    externals: {
         'react': {
           root: 'React',
           common2js: 'react',
           commonjs: 'react',
           amd: 'react'
         }
-    }]
+    }
 };
 
 module.exports = config;
